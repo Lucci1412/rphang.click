355c05b16860e32b276e3c654c47b2f931cb8b7f
Lucci1412 <66712654+Lucci1412@users.noreply.github.com>
2025-05-20 19:20:07 +0700

    fix: some issuee

--- src/modules/movie-detail/ui/view/movie-detail-view.tsx
+++ src/modules/movie-detail/ui/view/movie-detail-view.tsx
@@ -1,27 +1,27 @@
 "use client";
 import { trpc } from "@/trpc/client";
 import { MovieDetailOutput } from "../type";
-import NoMoviesFound from "@/components/no-movie-found";
-import { ErrorBoundary } from "react-error-boundary";
+// import NoMoviesFound from "@/components/no-movie-found";
+// import { ErrorBoundary } from "react-error-boundary";
 import MovieHeroSession from "../session/movie-hero-session";
 import MovieDetailSession from "../session/movie-detail-session";
 // import SimilarSession from "../session/similar-session";
-import MovieHeroSkeleton from "../components/skeleton/movie-hero-skeleton";
+// import MovieHeroSkeleton from "../components/skeleton/movie-hero-skeleton";
 // import DetailSkeleton from "../components/skeleton/movie-detail-skeleton";
 // import SimilarSkeleton from "../components/skeleton/similar-skeleton";
 import SimilarSession from "../session/similar-session";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 
 interface MovieDetailViewProps {
   slug: string;
 }
 export const MovieDetailView = ({ slug }: MovieDetailViewProps) => {
   return (
-    <Suspense fallback={<MovieDetailSkeleton />}>
-      <ErrorBoundary fallback={<NoMoviesFound />}>
+    // <Suspense fallback={<MovieDetailSkeleton />}>
+    //   <ErrorBoundary fallback={<NoMoviesFound />}>
         <MovieDetailViewSuspense slug={slug} />
-      </ErrorBoundary>
-    </Suspense>
+    //   </ErrorBoundary>
+    // </Suspense>
   );
 };
 const MovieDetailViewSuspense = ({ slug }: MovieDetailViewProps) => {
@@ -37,12 +37,12 @@ const MovieDetailViewSuspense = ({ slug }: MovieDetailViewProps) => {
 };
 
 export default MovieDetailView;
-const MovieDetailSkeleton = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <MovieHeroSkeleton></MovieHeroSkeleton>
-      {/* <DetailSkeleton></DetailSkeleton>
-      <SimilarSkeleton></SimilarSkeleton> */}
-    </div>
-  );
-};
+// const MovieDetailSkeleton = () => {
+//   return (
+//     <div className="min-h-screen bg-background">
+//       <MovieHeroSkeleton></MovieHeroSkeleton>
+//       {/* <DetailSkeleton></DetailSkeleton>
+//       <SimilarSkeleton></SimilarSkeleton> */}
+//     </div>
+//   );
+// };
